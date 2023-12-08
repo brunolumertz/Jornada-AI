9b4578a156451773a879b03f8942637cc32feb0d
brunolumertz <57117496+brunolumertz@users.noreply.github.com>
2023-12-08 19:54:47 -0300

    Aula 5
    Continuando os scripts e treinamento.

--- script.js
+++ script.js
@@ -18,3 +18,69 @@ function fillDinoList () {
     }
     console.log('Lista de dino criada')
 }
+
+setTimeout(() => {
+    fillDinoList()
+    controls.dispatch('jump')
+}, 1000)
+
+setInterval(() => {
+    if (!game.activated) return
+
+    const dino = dinoList[dinoIndex]
+
+    if (game.crashed) { //salva o novo melhor score
+        if (dino.score > bestScore) {
+            bestScore = dino.score
+            bestRNA = dino.save()
+            console.log('Melhor pontuacao:', bestScore)
+        }
+        dinoIndex++
+
+        if (dinoIndex === SAMPLES) {
+            fillDinoList();
+            dinoIndex = 0
+            bestScore = 0
+        }
+        game.restart()
+    }
+
+    const {tRex, horizon, currentSpeed, distanceRan, dimensions} = game
+    dino.score = distanceRan - 2000
+
+    const player = {
+        x: tRex.xPos,
+        y: tRex.yPos,
+        speed: currentSpeed
+    };
+
+    const [obstacle] = horizon.obstacles
+    .map((obstacles) => {
+        return{
+            x: obstacle.xPos,
+            y: obstacle.yPos
+        }
+    })
+    .filter((obstacle) => obstacle.x > player.x)
+
+    if (obstacle){
+        const distance = 1 - (utils.getDistance(player, obstacle) / dimensions.WIDTH);
+        const speed = player.speed / 6
+        const height = Math.tanh(105 - obstacle.y)
+
+        const [jump, crouch] = dino.compute([
+            distance,
+            speed, 
+            height,
+        ]);
+
+        if (jump === crouch) return // condicoes e acoes
+        if (jump) controls.dispatch('jump')
+        if(crouch) controls.dispatch('crouch')
+    };
+}, 100);
+
+/* const s = document.createElement('script');
+s.type = 'module';
+s.src = 'http://localhost:5500/script.js'
+document.body.appendChild(s); */
